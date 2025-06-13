5a59a0554ff6587d3bb84ed7f95e01e8754f77ca
Marcos Paulo <marcos.poo@gmail.com>
2025-06-13 16:32:22 -0300

    feat: Criação do InfraModule.

--- RegerBiblioteca.Infra/InfraModule.cs
+++ RegerBiblioteca.Infra/InfraModule.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegerBiblioteca.Infra.Persistence;
 
 namespace RegerBiblioteca.Infra
 {
-    public class InfraModule
+    public static class InfraModule
     {
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddData(configuration)
+                .AddRepositories();
+
+            return services;
+        }
+        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("RegerBibliotecaCs");
+            services.AddDbContext<DBContext>(o => o.UseSqlServer(connectionString));
+
+            return services;
+        }
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddScoped<IAuthorRepository, AuthorRepository>();
+
+            return services;
+        }
     }
 }
