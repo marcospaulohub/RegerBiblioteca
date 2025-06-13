0fa48318f9ec553d7845cea775bb744b70356f0b
Marcos Paulo <marcos.poo@gmail.com>
2025-06-13 16:31:59 -0300

    feat: Criação do AutorMap

--- RegerBiblioteca.Infra/Persistence/Mappings/AutorMap.cs
+++ RegerBiblioteca.Infra/Persistence/Mappings/AutorMap.cs
@@ -8,7 +8,31 @@ namespace RegerBiblioteca.Infra.Persistence.Mappings
     {
         public void Configure(EntityTypeBuilder<Autor> builder)
         {
-            throw new NotImplementedException();
+            builder
+                .ToTable("Autor");
+
+            builder
+                .HasKey(a => a.Id);
+
+            builder
+                .Property(a => a.Nome)
+                .HasColumnType("varchar(200)");
+
+            builder
+                .Property(a => a.Nacionalidade)
+                .HasColumnType("varchar(100)");
+
+            builder
+                .Property(a => a.Biografia)
+                .HasColumnType("varchar(max)");
+
+            builder
+                .Property(a => a.FotoUrl)
+                .HasColumnType("varchar(200)");
+
+            builder
+                .Property(a => a.SiteUrl)
+                .HasColumnType("varchar(200)");
         }
     }
 }
