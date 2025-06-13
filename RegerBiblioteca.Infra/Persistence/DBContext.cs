ad543f88f365cd213ff213257457c2e486b7fed1
Marcos Paulo <marcos.poo@gmail.com>
2025-06-13 16:31:32 -0300

    feat: Criação do DbContext.

---
+++ RegerBiblioteca.Infra/Persistence/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegerBiblioteca.Core.Entities;
+using RegerBiblioteca.Infra.Persistence.Mappings;
+
+namespace RegerBiblioteca.Infra.Persistence
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options) { }
+
+        public DbSet<Autor> Autores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AutorMap());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
