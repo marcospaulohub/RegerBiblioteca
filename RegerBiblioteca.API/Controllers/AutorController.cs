338eb7b25731d32386433c113db43dae89991375
Marcos Paulo <marcos.poo@gmail.com>
2025-06-15 23:52:43 -0300

    feat: Adiciona AutorController.

---
+++ RegerBiblioteca.API/Controllers/AutorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegerBiblioteca.API.Controllers
+{
+    [Route("api/autor")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+    }
+}
