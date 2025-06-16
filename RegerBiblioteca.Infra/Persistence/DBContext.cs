bce02bef45564756c3774b045acadeb5acfaaaae
Marcos Paulo <marcos.poo@gmail.com>
2025-06-16 02:42:19 -0300

    feat: Adição de arquivos de Map.

--- RegerBiblioteca.Infra/Persistence/DBContext.cs
+++ RegerBiblioteca.Infra/Persistence/DBContext.cs
@@ -14,6 +14,14 @@ namespace RegerBiblioteca.Infra.Persistence
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfiguration(new AutorMap());
+            builder.ApplyConfiguration(new CategoriaMap());
+            builder.ApplyConfiguration(new EmprestimoMap());
+            builder.ApplyConfiguration(new ExemplarMap());
+            builder.ApplyConfiguration(new FuncionarioMap());
+            builder.ApplyConfiguration(new LivroAutorMap());
+            builder.ApplyConfiguration(new LivroCategoriaMap());
+            builder.ApplyConfiguration(new LivroMap());
+            builder.ApplyConfiguration(new UsuarioMap());
 
             base.OnModelCreating(builder);
         }
