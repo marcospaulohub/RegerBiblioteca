2a79494329d25c364bc3dc3afb5fc832c8ff5475
Marcos Paulo <marcos.poo@gmail.com>
2025-06-24 07:05:53 -0300

    feat: Adição do arquivo AppModule.

---
+++ RegerBiblioteca.App/AppModule.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using RegerBiblioteca.App.Services;
+using RegerBiblioteca.App.Services.Interfaces;
+using RegerBiblioteca.App.Validators.Autores;
+
+namespace RegerBiblioteca.App
+{
+    public static class AppModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddServices()
+                .AddValidation();
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAutorService, AutorService>();
+
+            return services;
+        }
+        private static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            services
+                .AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssemblyContaining<CreateAutorInputModelValidator>();
+
+            return services;
+        }
+    }
+}
