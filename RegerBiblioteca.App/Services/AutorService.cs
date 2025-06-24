51a6d9c3168bcac24d71f70616f278900992a7b8
Marcos Paulo <marcos.poo@gmail.com>
2025-06-24 07:03:07 -0300

    feat: Criação e implementação do AutorInputModel.

---
+++ RegerBiblioteca.App/Services/AutorService.cs
@@ -0,0 +1,79 @@
+﻿using RegerBiblioteca.App.Models.Autores;
+using RegerBiblioteca.App.Services.Interfaces;
+using RegerBiblioteca.Core.Entities;
+using RegerBiblioteca.Core.Repositories;
+
+namespace RegerBiblioteca.App.Services
+{
+    public class AutorService : IAutorService
+    {
+        private readonly IAutorRepository _autorRepository; 
+        public AutorService(IAutorRepository autorRepository)
+        {
+            _autorRepository = autorRepository;
+        }
+
+        public ResultViewModel<int> Create(CreateAutorInputModel model)
+        {
+            var autor = new Autor
+            {
+                Nome = model.Nome,
+                Nacionalidade = model.Nacionalidade,
+                DataNascimento = model.DataNascimento,
+                DataFalecimento = model.DataFalecimento,
+                Biografia = model.Biografia,
+                FotoUrl = model.FotoUrl,
+                SiteUrl = model.SiteUrl
+            };
+
+            var autorId = _autorRepository.Insert(autor);
+
+            if (autorId <= 0)
+                return ResultViewModel<int>.Error("Erro ao criar autor.");
+
+            return ResultViewModel<int>.Success(autorId);
+        }
+        public ResultViewModel Update(int id, UpdateAutorInputModel model)
+        {
+            var autor = _autorRepository.GetById(id);
+            if (autor == null)
+                return ResultViewModel.Error("Autor não encontrado.");
+
+            _autorRepository.Update(autor);
+
+            return ResultViewModel.Success();
+        }
+
+        public ResultViewModel Delete(int id)
+        {
+            var autor = _autorRepository.GetById(id);
+            if (autor == null)
+                return ResultViewModel.Error("Autor não encontrado.");
+
+            _autorRepository.Delete(autor);
+
+            return ResultViewModel.Success();
+        }
+        public ResultViewModel<AutorViewModel?> GetById(int id)
+        {
+            var autor = _autorRepository.GetById(id);
+
+            return autor is null ?
+                ResultViewModel<AutorViewModel?>.Error("Autor não encontrado.") :
+                ResultViewModel<AutorViewModel?>.Success(AutorViewModel.FromEntity(autor));
+        }
+
+        public ResultViewModel<List<AutorViewModel?>> GetAll()
+        {
+            var listaAutores = _autorRepository.GetAll();
+
+            var list = listaAutores
+                .Select(AutorViewModel.FromEntity)
+                .ToList();
+
+            return ResultViewModel<List<AutorViewModel?>>.Success(list);
+        }
+
+
+    }
+}
---
+++ RegerBiblioteca.App/Services/Interfaces/IAutorService.cs
@@ -0,0 +1,13 @@
+﻿using RegerBiblioteca.App.Models.Autores;
+
+namespace RegerBiblioteca.App.Services.Interfaces
+{
+    public interface IAutorService
+    {
+        ResultViewModel<int> Create(CreateAutorInputModel model);
+        ResultViewModel Update(int id, UpdateAutorInputModel model);
+        ResultViewModel Delete(int id);
+        ResultViewModel<AutorViewModel?> GetById(int id);
+        ResultViewModel<List<AutorViewModel?>> GetAll();
+    }
+}
