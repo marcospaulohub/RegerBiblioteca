a785b26b9875e3c98309371d73794d336fc64e3a
Marcos Paulo <marcos.poo@gmail.com>
2025-07-11 16:12:37 -0300

    feat: Implementado o UsuarioService.

---
+++ RegerBiblioteca.App/Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using RegerBiblioteca.App.Models.Reservas;
+using RegerBiblioteca.App.Models.Usuarios;
+
+namespace RegerBiblioteca.App.Services.Interfaces
+{
+    public interface IUsuarioService
+    {
+        ResultViewModel<int> Create(CreateUsuarioInputModel model);
+        ResultViewModel Update(int id, UpdateUsuarioInputModel model);
+        ResultViewModel Delete(int id);
+        ResultViewModel<UsuarioViewModel?> GetById(int id);
+        ResultViewModel<List<UsuarioViewModel?>> GetAll();
+    }
+}
---
+++ RegerBiblioteca.App/Services/UsuarioService.cs
@@ -0,0 +1,85 @@
+﻿using RegerBiblioteca.App.Models.Usuarios;
+using RegerBiblioteca.App.Services.Interfaces;
+using RegerBiblioteca.Core.Entities;
+using RegerBiblioteca.Core.Repositories;
+
+namespace RegerBiblioteca.App.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public ResultViewModel<int> Create(CreateUsuarioInputModel model)
+        {
+            var usuario = new Usuario
+            {
+                Nome = model.Nome,
+                Email = model.Email,
+                Senha = model.Senha,
+                TipoUsuario = model.TipoUsuario
+            };
+
+            var usuarioId = _usuarioRepository.Insert(usuario); 
+            if (usuarioId <= 0)
+            {
+                return ResultViewModel<int>.Error("Erro ao criar usuário.");
+            }
+
+            return ResultViewModel<int>.Success(usuarioId);
+        }
+
+        public ResultViewModel Update(int id, UpdateUsuarioInputModel model)
+        {
+            var usuario = _usuarioRepository.GetById(id);
+
+            if (usuario is null)
+                return ResultViewModel.Error("Usuário não encontrado.");
+
+            usuario.Nome = model.Nome;
+            usuario.Email = model.Email;
+            usuario.Senha = model.Senha;
+            usuario.TipoUsuario = model.TipoUsuario;
+            
+            _usuarioRepository.Update(usuario);
+            
+            return ResultViewModel.Success();
+        }
+
+        public ResultViewModel Delete(int id)
+        {
+            var usuario = _usuarioRepository.GetById(id);
+
+            if (usuario is null)
+                return ResultViewModel.Error("Usuário não encontrado.");
+
+            _usuarioRepository.Delete(usuario);
+
+            return ResultViewModel.Success();
+        }
+
+        public ResultViewModel<UsuarioViewModel?> GetById(int id)
+        {
+            var usuario = _usuarioRepository.GetById(id);
+
+            return usuario is null 
+                ? ResultViewModel<UsuarioViewModel?>.Error("Usuário não encontrado.")
+                : ResultViewModel<UsuarioViewModel?>.Success(UsuarioViewModel.FromEntity(usuario));
+        }
+
+        public ResultViewModel<List<UsuarioViewModel?>> GetAll()
+        {
+            var listaUsuarios = _usuarioRepository.GetAll();
+
+            var lista = listaUsuarios
+                .Select(u => UsuarioViewModel.FromEntity(u))
+                .ToList();
+
+            return ResultViewModel<List<UsuarioViewModel?>>.Success(lista);
+        }
+    }
+}
