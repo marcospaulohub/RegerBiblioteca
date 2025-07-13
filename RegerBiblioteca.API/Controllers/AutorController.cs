633eee9cdb2f633073bd6173661664ee528469a7
Marcos Paulo <marcos.poo@gmail.com>
2025-07-12 23:59:13 -0300

    feat: Implementado o AutorController.

--- RegerBiblioteca.API/Controllers/AutorController.cs
+++ RegerBiblioteca.API/Controllers/AutorController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RegerBiblioteca.App.Models.Autores;
+using RegerBiblioteca.App.Services.Interfaces;
 
 namespace RegerBiblioteca.API.Controllers
 {
@@ -6,5 +8,63 @@ namespace RegerBiblioteca.API.Controllers
     [ApiController]
     public class AutorController : ControllerBase
     {
+        private readonly IAutorService _autorService;
+
+        public AutorController(IAutorService autorService)
+        {
+            _autorService = autorService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateAutorInputModel model)
+        {
+            var result = _autorService.Create(model);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, result);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Patch(int id, UpdateAutorInputModel model)
+        {
+            var result = _autorService.Update(id, model);
+
+            if (!result.IsSuccess)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _autorService.Delete(id);
+
+            if (!result.IsSuccess)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _autorService.GetById(id);
+
+            if (!result.IsSuccess)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _autorService.GetAll();
+
+            if(!result.IsSuccess)
+                return NotFound();
+
+            return Ok(result);
+        }
     }
 }
