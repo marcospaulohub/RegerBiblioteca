2bc813c3b189556abf2a738de4f8c29ca0e2185a
Marcos Paulo <marcos.poo@gmail.com>
2025-09-02 17:00:16 -0300

    feat: Criação das mensagens de validação para Autor.

---
+++ RegerBiblioteca.Core/Messages/AutorMessages/AutorMsgs.cs
@@ -0,0 +1,37 @@
+﻿namespace RegerBiblioteca.Core.Messages.AutorMessages
+{
+    public static class AutorMsgs
+    {
+        private const string fileNameResx = "RegerBiblioteca.Core.Messages.AutorMessages.AutorMsgsResource";
+
+        public static string GetNomeNotEmpty()
+        {
+            return BaseMsgs.GetResource("NomeNotEmpty", fileNameResx);
+        }
+
+        public static string GetNomeMaxLength()
+        {
+            return BaseMsgs.GetResource("NomeMaxLength", fileNameResx);
+        }
+        
+        public static string GetNacionalidadeMaxLength()
+        {
+            return BaseMsgs.GetResource("NacionalidadeMaxLength", fileNameResx);
+        }
+
+        public static string GetNascimentoInvalid()
+        {
+            return BaseMsgs.GetResource("NascimentoInvalid", fileNameResx);
+        }
+
+        public static string GetFotoUrlMaxLength()
+        {
+            return BaseMsgs.GetResource("FotoUrlMaxLength", fileNameResx);
+        }
+
+        public static string GetSiteUrlMaxLength()
+        {
+            return BaseMsgs.GetResource("SiteUrlMaxLength", fileNameResx);
+        }
+    }
+}
