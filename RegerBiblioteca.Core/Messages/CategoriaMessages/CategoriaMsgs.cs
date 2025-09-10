590f310c7a7fca3084057ba02caf16bea66b3f53
Marcos Paulo <marcos.poo@gmail.com>
2025-09-10 16:00:08 -0300

    feat: Criação das mensagens de validação para Categoria.

---
+++ RegerBiblioteca.Core/Messages/CategoriaMessages/CategoriaMsgs.cs
@@ -0,0 +1,37 @@
+﻿namespace RegerBiblioteca.Core.Messages.CategoriaMessages
+{
+    public static class CategoriaMsgs
+    {
+        private const string fileNameResx = "RegerBiblioteca.Core.Messages.CategoriaMessages.CategoriaMsgsResource";
+
+        public static string GetNomeNotEmpty()
+        {
+            return BaseMsgs.GetResource("NomeNotEmpty", fileNameResx);
+        }
+
+        public static string GetNomeMaxLength()
+        {
+            return BaseMsgs.GetResource("NomeMaxLength", fileNameResx);
+        }
+
+        public static string GetDescricaoMaxLength()
+        {
+            return BaseMsgs.GetResource("DescricaoMaxLength", fileNameResx);
+        }
+
+        public static string GetGeneroInvalid()
+        {
+            return BaseMsgs.GetResource("GeneroInvalid", fileNameResx);
+        }
+
+        public static string GetAtivoNotNull()
+        {
+            return BaseMsgs.GetResource("AtivoNotNull", fileNameResx);
+        }
+
+        public static string GetAtivoInvalid()
+        {
+            return BaseMsgs.GetResource("AtivoInvalid", fileNameResx);
+        }
+    }
+}
