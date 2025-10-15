80afc54ad426881aa1db0fbf42701ab391c090cf
Marcos Paulo <marcos.poo@gmail.com>
2025-10-15 03:42:50 -0300

    feat: Criação de InputModel para alteração de Senha.

---
+++ RegerBiblioteca.Core/ViewModels/Usuarios/AlteraSenhaInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RegerBiblioteca.Core.ViewModels.Usuarios
+{
+    public class AlteraSenhaInputModel
+    {
+        public string Email { get; set; }
+        public string SenhaAtual { get; set; }
+        public string NovaSenha { get; set; }
+    }
+}
