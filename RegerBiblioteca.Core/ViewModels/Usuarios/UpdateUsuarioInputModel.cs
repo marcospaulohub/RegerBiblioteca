8418ab6e5853cdc4fd422aabedc67cfcaddea7fd
Marcos Paulo <marcos.poo@gmail.com>
2025-10-16 14:52:56 -0300

    refactor: Alteração dos tipos Email e Senha para string.

--- RegerBiblioteca.Core/ViewModels/Usuarios/UpdateUsuarioInputModel.cs
+++ RegerBiblioteca.Core/ViewModels/Usuarios/UpdateUsuarioInputModel.cs
@@ -6,8 +6,8 @@ namespace RegerBiblioteca.Core.ViewModels.Usuarios
     public class UpdateUsuarioInputModel
     {
         public string Nome { get; set; }
-        public Email Email { get; set; }
-        public Senha Senha { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
         public TipoUsuario TipoUsuario { get; set; }
     }
 }
